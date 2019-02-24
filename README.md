15d653df6b17cbd547170bcd3f41eee31f585420
Ryz Jenvey <rjenvey@hotmail.co.uk>
2019-02-24 11:10:24 +0000

    add touch to start to prevent vibration console errors.
    add disable option for vibration.
    simplify error scenario.

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ A customisable interval timer which will display a random symbol at each interva
 
 Also vibrates on interval for mobile devices.
 
-Customise with URL query parameters.
+Customise with URL query parameters. Touch or click screen to start.
 
-| Parameter | Use                                                           |
-|-----------|---------------------------------------------------------------|
-| symbols   | comma seperated list of items to randomly display at interval |
-| interval  | interval in seconds                                           |
-| totalTime | total time in seconds                                         |
+| Parameter        | Use                                                           |
+|------------------|---------------------------------------------------------------|
+| symbols          | comma seperated list of items to randomly display at interval |
+| interval         | interval in seconds                                           |
+| totalTime        | total time in seconds                                         |
+| disableVibration | boolean to disable vibration                                  |
 
 e.g.
 
--- controller.js
+++ controller.js
@@ -1,34 +1,37 @@
 const getRandom = (list) => list[Math.floor(Math.random() * list.length)]
 const printAtId = (id, s) => document.getElementById(id).innerHTML = s
 
-const symbolId = 'symbol'
-
 if (window.location.search.length === 0) {
-    printAtId(symbolId,
-        `please ensure you have added symbols, 
-        interval and total time as URL query parameters 
-        e.g. ?symbols=a,b,c&interval=2&totalTime=10`)
-    throw('')
+    window.location.assign('index.html?symbols=a,b,c&interval=2&totalTime=10')
+    alert('No parameters added. Default URL params assigned')
 }
 
+const symbolId = 'symbol'
 const searchParams = new URLSearchParams(window.location.search);
 const symbols = searchParams.get('symbols').split(',');
 const interval = searchParams.get('interval')
+const disableVibrate = searchParams.get('disableVibration') === 'true'
 let totalTime = searchParams.get('totalTime')
 
-let timerId
-navigator.vibrate(150);
-timerId = setInterval(() => {
-    printAtId('totalTime', totalTime)
-    if (totalTime === 0) {
-        clearInterval(timerId)
-        printAtId(symbolId, '')
-        navigator.vibrate([150,30,150,30,150]);
-    }
-    else if (totalTime % interval === 0) {
-        printAtId(symbolId, getRandom(symbols))
-        navigator.vibrate(150);
-    }
-    totalTime--
-}, 1000);
+const startTimer = () => {
+    let timerId
+    vibrate()
+    timerId = setInterval(() => {
+        printAtId('totalTime', totalTime)
+        if (totalTime === 0) {
+            clearInterval(timerId)
+            printAtId(symbolId, '')
+            vibrate([150, 30, 150, 30, 150])
+        } else if (totalTime % interval === 0) {
+            printAtId(symbolId, getRandom(symbols))
+            vibrate()
+        }
+        totalTime--
+    }, 1000);
+}
 
+const vibrate = (pattern = 150) => {
+    if (!disableVibrate) {
+        navigator.vibrate(pattern)
+    }
+};
